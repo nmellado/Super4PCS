f8c585d57b0e9b4e64713ff61972289d8198bbc5
Nicolas Mellado <nmellado0@gmail.com>
2014-10-21 17:27:10 +0100

    Add resume of the test

--- tests/pair_extraction.cc
+++ tests/pair_extraction.cc
@@ -16,32 +16,36 @@
 //
 // Authors: Nicolas Mellado
 //
-// An implementation of the Super 4-points Congruent Sets (Super 4PCS) 
-// algorithm presented in:
+// This test check the validity of the pair extraction subroutine on random data
+// of different dimensions (2,3 and 4).
+//
+//
+// This test is part of the implementation of the Super 4-points Congruent Sets
+// (Super 4PCS) algorithm presented in:
 //
 // Super 4PCS: Fast Global Pointcloud Registration via Smart Indexing
 // Nicolas Mellado, Dror Aiger, Niloy J. Mitra
 // Symposium on Geometry Processing 2014.
 //
-// Data acquisition in large-scale scenes regularly involves accumulating 
-// information across multiple scans. A common approach is to locally align scan 
-// pairs using Iterative Closest Point (ICP) algorithm (or its variants), but 
-// requires static scenes and small motion between scan pairs. This prevents 
+// Data acquisition in large-scale scenes regularly involves accumulating
+// information across multiple scans. A common approach is to locally align scan
+// pairs using Iterative Closest Point (ICP) algorithm (or its variants), but
+// requires static scenes and small motion between scan pairs. This prevents
 // accumulating data across multiple scan sessions and/or different acquisition
 // modalities (e.g., stereo, depth scans). Alternatively, one can use a global
-// registration algorithm allowing scans to be in arbitrary initial poses. The 
+// registration algorithm allowing scans to be in arbitrary initial poses. The
 // state-of-the-art global registration algorithm, 4PCS, however has a quadratic
-// time complexity in the number of data points. This vastly limits its 
+// time complexity in the number of data points. This vastly limits its
 // applicability to acquisition of large environments. We present Super 4PCS for
-// global pointcloud registration that is optimal, i.e., runs in linear time (in 
-// the number of data points) and is also output sensitive in the complexity of 
-// the alignment problem based on the (unknown) overlap across scan pairs. 
-// Technically, we map the algorithm as an ‘instance problem’ and solve it 
-// efficiently using a smart indexing data organization. The algorithm is 
+// global pointcloud registration that is optimal, i.e., runs in linear time (in
+// the number of data points) and is also output sensitive in the complexity of
+// the alignment problem based on the (unknown) overlap across scan pairs.
+// Technically, we map the algorithm as an ‘instance problem’ and solve it
+// efficiently using a smart indexing data organization. The algorithm is
 // simple, memory-efficient, and fast. We demonstrate that Super 4PCS results in
-// significant speedup over alternative approaches and allows unstructured 
-// efficient acquisition of scenes at scales previously not possible. Complete 
-// source code and datasets are available for research use at 
+// significant speedup over alternative approaches and allows unstructured
+// efficient acquisition of scenes at scales previously not possible. Complete
+// source code and datasets are available for research use at
 // http://geometry.cs.ucl.ac.uk/projects/2014/super4PCS/.
 
 #include "4pcs.h"
