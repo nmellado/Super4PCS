72ff3cf4924ad8a021c48a7c25becbce375f433b
Nicolas Mellado <nmellado0@gmail.com>
2016-05-26 17:50:16 +0200

    Add configuration example demonstrating the algorithm efficiency for dense sampling.

--- run-example.sh
+++ run-example.sh
@@ -6,6 +6,7 @@
 # -d delta, used to compute the LCP between the two models
 # -t maximum computation time in seconds
 # -n number of samples used for the matching
-time -p ./build/Super4PCS -i hippo1.obj hippo2.obj -o 0.7 -d 0.01 -t 1000 -n 400 -r super4pcs.obj
+time -p ./build/Super4PCS -i hippo1.obj hippo2.obj -o 0.7 -d 0.01 -t 1000 -n 400 -r super4pcs_fast.obj
+time -p ./build/Super4PCS -i hippo1.obj hippo2.obj -o 0.75 -d 0.001 -t 1000 -n 4000 -r super4pcs_extreme.obj
 time -p ./build/Super4PCS -i hippo1.obj hippo2.obj -o 0.7 -d 0.01 -t 1000 -n 400 -r 4pcs.obj -x
 
