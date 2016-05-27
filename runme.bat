39715175fd2039b266825812f7b3ad690f87f3bc
Nicolas Mellado <Nicolas Mellado@DESKTOP-LHN29H2>
2016-05-27 11:37:30 +0200

    Update test file to output computed matrix

--- runme.bat
+++ runme.bat
@@ -1,3 +1,3 @@
-@ x64\Release\Super4pcs -i hippo1.obj hippo2.obj -o 0.7 -d 0.01 -t 1000 -n 400 -r super4pcs_fast.obj
-@ x64\Release\Super4pcs -i hippo1.obj hippo2.obj -o 0.75 -d 0.001 -t 1000 -n 4000 -r super4pcs_extreme.obj
+@ x64\Release\Super4pcs -i hippo1.obj hippo2.obj -o 0.7 -d 0.01 -t 1000 -n 400 -r super4pcs_fast.obj -m mat_super4pcs_fast.txt
+@ x64\Release\Super4pcs -i hippo1.obj hippo2.obj -o 0.75 -d 0.001 -t 1000 -n 4000 -r super4pcs_extreme.obj -m mat_super4pcs_extreme.txt
 @ PAUSE
