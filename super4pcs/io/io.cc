nmellado/Super4PCS

commit d2d82e2b53942aae8c81e559f197f72df68c3717

Remove comments about kinect.

super4pcs/io/io.cc
@@ -345,7 +345,7 @@ IOManager::WritePly(string filename,
   // Write Header
   plyFile << "ply" << std::endl;
   plyFile << "format binary_little_endian 1.0" << std::endl;
-  plyFile << "comment Kinect depth data" << std::endl;
+  plyFile << "Super4PCS output file" << std::endl;
   plyFile << "element vertex " << v.size() << std::endl;
   plyFile << "property float x" << std::endl;
   plyFile << "property float y" << std::endl;

commit 0e24ba962edee40d9fc98a24e6c3a6581f2a5a65

Fix stupid bug introduced in commit d2d82e2. Should fix #16.

super4pcs/io/io.cc
@@ -345,7 +345,7 @@ IOManager::WritePly(string filename,
   // Write Header
   plyFile << "ply" << std::endl;
   plyFile << "format binary_little_endian 1.0" << std::endl;
-  plyFile << "Super4PCS output file" << std::endl;
+  plyFile << "comment Super4PCS output file" << std::endl;
   plyFile << "element vertex " << v.size() << std::endl;
   plyFile << "property float x" << std::endl;
   plyFile << "property float y" << std::endl;