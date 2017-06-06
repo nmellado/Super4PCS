462c8c651cc3ccb47526f04a3b7baa2195babae4
Nicolas Mellado <nmellado0@gmail.com>
2017-06-06 15:42:27 +0200

    Update run scripts for better comparison with 4pcs

--- run-example.sh
+++ run-example.sh
@@ -6,7 +6,7 @@
 # -d delta, used to compute the LCP between the two models
 # -t maximum computation time in seconds
 # -n number of samples used for the matching
-time -p ./build/Super4PCS -i hippo1.obj hippo2.obj -o 0.7 -d 0.01 -t 1000 -n 400 -r super4pcs_fast.obj -m mat_super4pcs_fast.txt
+time -p ./build/Super4PCS -i hippo1.obj hippo2.obj -o 0.7 -d 0.005 -t 1000 -n 200 -r super4pcs_fast.obj -m mat_super4pcs_fast.txt 
+time -p ./build/Super4PCS -i hippo1.obj hippo2.obj -o 0.7 -d 0.005 -t 1000 -n 200 -r      4pcs_fast.obj -m      mat_4pcs_fast.txt -x
 time -p ./build/Super4PCS -i hippo1.obj hippo2.obj -o 0.75 -d 0.001 -t 1000 -n 4000 -r super4pcs_extreme.obj -m mat_super4pcs_extreme.txt
-time -p ./build/Super4PCS -i hippo1.obj hippo2.obj -o 0.7 -d 0.01 -t 1000 -n 400 -r 4pcs.obj -x -m mat_r4pcs.txt
 
