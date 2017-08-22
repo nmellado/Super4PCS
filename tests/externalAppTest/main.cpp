75671cefddf2b4535cbb1f998a3080090bfaf1f2
Nicolas <nmellado0@gmail.com>
2017-08-22 19:28:39 +0200

    Add new test externalAppTestTarget, which compiles and runs an external app using the super4CPS library. In order to avoid issues with dynamic linking after installation, the application RPATH is forced to the Super4PCS library installation folder. Forces the application to be installed, by depending on target ForceSuper4PCSInstallation

---
+++ tests/externalAppTest/main.cpp
@@ -0,0 +1,50 @@
+#include "super4pcs/algorithms/super4pcs.h"
+#include "super4pcs/io/io.h"
+#include "super4pcs/utils/geometry.h"
+
+#include <Eigen/Dense>
+
+
+int main(int argc, char **argv) {
+  using namespace GlobalRegistration;
+  using namespace std;
+
+  vector<Point3D> set1, set2;
+  vector<Eigen::Matrix2f> tex_coords1, tex_coords2;
+  vector<typename Point3D::VectorType> normals1, normals2;
+  vector<tripple> tris1, tris2;
+  vector<std::string> mtls1, mtls2;
+
+  IOManager iomanager;
+
+  // dummy call, to test symbols accessibility
+  iomanager.ReadObject("", set1, tex_coords1, normals1, tris1, mtls1);
+
+  // check availability of the Utils functions
+  if (tris1.size() == 0)
+    Utils::CleanInvalidNormals(set1, normals1);
+
+  // Our matcher.
+  Match4PCSOptions options;
+
+  // Set parameters.
+  Match4PCSBase::MatrixType mat;
+  double overlap;
+  options.configureOverlap(overlap);
+  
+  typename Point3D::Scalar score = 0;
+
+  constexpr Utils::LogLevel loglvl = Utils::Verbose;
+  using TrVisitorType = Match4PCSBase::DummyTransformVisitor;
+  Utils::Logger logger(loglvl);
+  
+  MatchSuper4PCS matcher(options, logger);
+  score = matcher.ComputeTransformation<TrVisitorType>(set1, &set2, mat);
+
+  logger.Log<Utils::Verbose>( "Score: ", score );
+
+  iomanager.WriteMatrix("output.map", mat.cast<double>(), IOManager::POLYWORKS);
+
+  return 0;
+}
+
