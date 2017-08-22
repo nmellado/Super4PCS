nmellado/Super4PCS

commit 93e9fbf0ea6b5976fbf24487480725614e708ebd

Remove compilation warnings in IO module

src/super4pcs/io/io_ply.h
@@ -1,6 +1,8 @@
 #ifndef IO_PLY_H
 #define IO_PLY_H
 
+#include <cstdarg>
+
 typedef enum {
     BINARY_BIG_ENDIAN_1,
     BINARY_LITTLE_ENDIAN_1,
@@ -138,6 +140,22 @@ bigLittleEndianSwap (T * v, unsigned int numOfElements)
     }
 }
 
+inline bool
+almostsafefread ( void * ptr, size_t size, size_t count, FILE * stream ){
+    size_t result = fread (ptr,size,count,stream);
+    return (result == count);
+}
+
+template<int targetCount = 1>
+inline bool
+almostsafefscanf ( FILE * stream, const char * format, ... ){
+    va_list args;
+    va_start(args, format);
+    int count = fscanf (stream,format, args);
+    va_end(args);
+    return (count == targetCount);
+}
+
 
 bool
 readBinary1Body (const std::string & filename,
@@ -151,7 +169,6 @@ readBinary1Body (const std::string & filename,
                  std::vector<typename GlobalRegistration::Point3D::VectorType>& normal,
                  std::vector<tripple>& face )
 {
-    //size_t count;
     using namespace std;
     using namespace GlobalRegistration;
 
@@ -161,9 +178,9 @@ readBinary1Body (const std::string & filename,
         return false;
     }
 
+    char c;
     for (unsigned int i = 0; i < headerSize; i++) {
-        char c;
-        /*count = */fread (&c, 1, 1, in);
+        almostsafefread (&c, 1, 1, in);
     }
 
     // *****************
@@ -176,20 +193,20 @@ readBinary1Body (const std::string & filename,
 
     for (unsigned int i = 0; i < numOfVertices && !feof (in); i++) {
         if (numOfVertexProperties==10){
-            fread (v, 4, 6, in);
-            fread (rgb_buff, sizeof(unsigned char), 4, in);
+            almostsafefread (v, 4, 6, in);
+            almostsafefread (rgb_buff, sizeof(unsigned char), 4, in);
         }else if (numOfVertexProperties==9){
-            fread (v, 4, 6, in);
-            fread (rgb_buff, sizeof(unsigned char), 3, in);
+            almostsafefread (v, 4, 6, in);
+            almostsafefread (rgb_buff, sizeof(unsigned char), 3, in);
         }else if (numOfVertexProperties==6 && haveColor){
-            fread (v, 4, 3, in);
-            fread (rgb_buff, sizeof(unsigned char), 3, in);
+            almostsafefread (v, 4, 3, in);
+            almostsafefread (rgb_buff, sizeof(unsigned char), 3, in);
         }else if (numOfVertexProperties==7 ){
-            fread (v, 4, 3, in);
-            fread (rgb_buff, sizeof(unsigned char), 4, in);
+            almostsafefread (v, 4, 3, in);
+            almostsafefread (rgb_buff, sizeof(unsigned char), 4, in);
         }
         else
-            fread (v, 4, numOfVertexProperties, in);
+            almostsafefread (v, 4, numOfVertexProperties, in);
         if (bigEndian == true)
             bigLittleEndianSwap (v, numOfVertexProperties);
         vertex.emplace_back( v[0],v[1],v[2] );
@@ -228,8 +245,8 @@ readBinary1Body (const std::string & filename,
         for (unsigned int i = 0; i < numOfFaces && !feof (in); i++) {
             unsigned int f[4];
             char polygonSize;
-            /*count = */fread (&polygonSize, 1, 1, in);
-            /*count = */fread (f, 4, 3, in);
+            almostsafefread (&polygonSize, 1, 1, in);
+            almostsafefread (f, 4, 3, in);
             if (bigEndian == true)
                 bigLittleEndianSwap (f, 3);
             face.emplace_back(f[0],f[1],f[2]);
@@ -260,9 +277,9 @@ readASCII1Body (const std::string & filename,
         return false;
     }
 
+    char c;
     for (unsigned int i = 0; i < headerSize; i++) {
-        char c;
-        /*count = */fread (&c, 1, 1, in);
+        almostsafefread (&c, 1, 1, in);
     }
 
     // *****************
@@ -276,29 +293,29 @@ readASCII1Body (const std::string & filename,
 
         if (numOfVertexProperties==10){
             for (unsigned int j = 0;  j < 6;  j++)
-                fscanf (in, "%f", &v[j]);
+                almostsafefscanf<1> (in, "%f", &v[j]);
             for (unsigned int j = 0;  j < 4;  j++)
-                fscanf (in, "%i", &rgb_buff[j]);
+                almostsafefscanf<1> (in, "%i", &rgb_buff[j]);
         }
         else if (numOfVertexProperties==9){
             for (unsigned int j = 0;  j < 6;  j++)
-                fscanf (in, "%f", &v[j]);
+                almostsafefscanf<1> (in, "%f", &v[j]);
             for (unsigned int j = 0;  j < 3;  j++)
-                fscanf (in, "%i", &rgb_buff[j]);
+                almostsafefscanf<1> (in, "%i", &rgb_buff[j]);
         }else if (numOfVertexProperties==6 && haveColor){
             for (unsigned int j = 0;  j < 3;  j++)
-                fscanf (in, "%f", &v[j]);
+                almostsafefscanf<1> (in, "%f", &v[j]);
             for (unsigned int j = 0;  j < 3;  j++)
-                fscanf (in, "%i", &rgb_buff[j]);
+                almostsafefscanf<1> (in, "%i", &rgb_buff[j]);
         }else if (numOfVertexProperties==7){
             for (unsigned int j = 0;  j < 3;  j++)
-                fscanf (in, "%f", &v[j]);
+                almostsafefscanf<1> (in, "%f", &v[j]);
             for (unsigned int j = 0;  j < 4;  j++)
-                fscanf (in, "%i", &rgb_buff[j]);
+                almostsafefscanf<1> (in, "%i", &rgb_buff[j]);
         }
         else
             for (unsigned int j = 0;  j < numOfVertexProperties;  j++)
-                fscanf (in, "%f", &v[j]);
+                almostsafefscanf<1> (in, "%f", &v[j]);
 
         vertex.emplace_back( v[0],v[1],v[2] );
 
@@ -336,7 +353,7 @@ readASCII1Body (const std::string & filename,
         for (unsigned int i = 0; i < numOfFaces && !feof (in); i++) {
             int f[3];
             int polygonSize;
-            /*count = */fscanf (in, "%d %d %d %d", &polygonSize, &f[0], &f[1], &f[2]);
+            almostsafefscanf<4> (in, "%d %d %d %d", &polygonSize, &f[0], &f[1], &f[2]);
             face.emplace_back(f[0],f[1],f[2]);
         }
     }

commit c8ad84e0460e0fc8efe8a98b169118590c8fd662

Fix bug introduced in 93e9fbf on variadic parameters forwarding.

src/super4pcs/io/io_ply.h
@@ -146,13 +146,10 @@ almostsafefread ( void * ptr, size_t size, size_t count, FILE * stream ){
     return (result == count);
 }
 
-template<int targetCount = 1>
+template<int targetCount, typename... Args>
 inline bool
-almostsafefscanf ( FILE * stream, const char * format, ... ){
-    va_list args;
-    va_start(args, format);
-    int count = fscanf (stream,format, args);
-    va_end(args);
+almostsafefscanf ( FILE * stream, const char * format, Args&&... args ){
+    int count = fscanf (stream,format, std::forward<Args>(args)...);
     return (count == targetCount);
 }
 